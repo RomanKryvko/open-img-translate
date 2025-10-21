89eedb548898c669a6c40234504a96f10b464629
Roman Kryvko <roman.kr43@gmail.com>
2025-10-21 22:08:48 +0200

    Use vite to bundle code.

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  build: {
+    outDir: "dist",
+    emptyOutDir: true,
+
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, "src/index.ts"),
+        content: resolve(__dirname, "src/content.ts"),
+      },
+      output: {
+        entryFileNames: "[name].js",
+        format: "es",
+      },
+    },
+  },
+});
