70cff0bfbb035846284bebe3d495c69b18bfc180
Roman Kryvko <roman.kr43@gmail.com>
2025-11-09 18:58:07 +0100

    Move hiding functionality to a separate component.

---
+++ src/popup/HideableOnClick.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { onMount, onDestroy } from "svelte";
+
+  const props = $props();
+  let element: HTMLElement | undefined = $state();
+  let visible = $state(true);
+
+  const hideOnClick = (e: Event) => {
+    if (element && !element.contains(<Node>e.target)) {
+      visible = false;
+    }
+  };
+
+  onMount(() => {
+    document.addEventListener("click", hideOnClick);
+  });
+
+  onDestroy(() => {
+    document.removeEventListener("click", hideOnClick);
+  });
+</script>
+
+{#if visible}
+  <div bind:this={element}>
+    {@render props.children?.()}
+  </div>
+{/if}
--- src/popup/TranslationPopup.svelte
+++ src/popup/TranslationPopup.svelte
@@ -1,32 +1,13 @@
 <script lang="ts">
-  import { onMount, onDestroy } from "svelte";
   import Draggable from "./Draggable.svelte";
   import Popup from "./Popup.svelte";
-
-  let popupElement: HTMLElement | undefined = $state();
+  import HideableOnClick from "./HideableOnClick.svelte";
 
   const props = $props();
-  let visible = $state(true);
-
-  const hideOnClick = (e: Event) => {
-    if (popupElement && !popupElement.contains(<Node>e.target)) {
-      visible = false;
-    }
-  };
-
-  onMount(() => {
-    document.addEventListener("click", hideOnClick);
-  });
-
-  onDestroy(() => {
-    document.removeEventListener("click", hideOnClick);
-  });
 </script>
 
-{#if visible}
-  <div bind:this={popupElement}>
-    <Draggable pos={props.pos}>
-      <Popup message={props.message} />
-    </Draggable>
-  </div>
-{/if}
+<HideableOnClick>
+  <Draggable pos={props.pos}>
+    <Popup message={props.message} />
+  </Draggable>
+</HideableOnClick>
